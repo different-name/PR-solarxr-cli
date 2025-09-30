f1c7ee1d4a3c3518771c5421e32215ccc69dd2e7
peelz <peelz.dev@gmail.com>
2025-09-29 21:40:39 -0400

    init

---
+++ solarxr-cli/build.rs
@@ -0,0 +1,16 @@
+use clap_complete::{generate_to, shells::Shell};
+use std::{env, io};
+
+include!("src/cli.rs");
+
+fn main() -> Result<(), io::Error> {
+    if let Some(out_dir) = env::var_os("OUT_DIR") {
+        let mut cmd = <Args as clap::CommandFactory>::command();
+        for shell in [Shell::Bash, Shell::Zsh, Shell::Fish] {
+            let path = generate_to(shell, &mut cmd, "solarxr-cli", &out_dir)?;
+            println!("cargo:warning=completion file generated: {path:?}");
+        }
+    }
+
+    Ok(())
+}
